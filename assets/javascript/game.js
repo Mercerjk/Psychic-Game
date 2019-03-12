f235910866b12921c1db62f2b46a2bb8102871a8
Josh Mercer <mercer.joshua.k@gmail.com>
2019-03-12 17:31:33 -0400

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,45 @@ var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 //variables
 let wins = 0;
 let losses = 0;
-let guesses = 9;
 let guessesLeft = 9;
-let guessedLetters = [];
+let playerGuess = [];
 
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    console.log(computerChoice)
-
-//computer chooses random letter
 document.onkeypress = function(event) {
+
     var userGuess = event.key;
 
     if(userGuess === computerChoice){
         wins++;
-    }else{
-        guesses--;
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        reset();
+    }
+    else {
+        guessesLeft = guessesLeft - 1;
+        playerGuess.push(userGuess);
+
+        if (guessesLeft ===0) {
+            losses++;
+            document.getElementById('losses').innerHTML = "Losses: " + losses;
+            reset();
+        }
     }
 
-    if(guesses === 0){
-        losses++
-    }
-    // Values and scores generated to HTML
+
+    document.getElementById('attempts').innerHTML = "Attemps Remaing: " + guessesLeft;
+    document.getElementById('playerGuess').innerHTML = "Guessed Letters:  " + playerGuess;
+
+};
+
+    // Values and scores generated to HTMLs
     document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "Losses: " + Losses;
-    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+    document.getElementById('attempts').innerHTML = "Attemps Remaing: " + guessesLeft;
+    document.getElementById('playerGuess').innerHTML = "Guessed Letters:  " + playerGuess;
 
-}
-      
+    function reset () {
+        guessesLeft = 9;
+        playerGuess = [];
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
